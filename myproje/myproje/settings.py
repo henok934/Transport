4d8de68e34457e79de5426a7ff5291b0691049bc
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2026-05-31 09:57:04 -0400

    change debug mode from True to False

--- myproje/myproje/settings.py
+++ myproje/myproje/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # 2. SECURITY SETTINGS...
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['*']
 
